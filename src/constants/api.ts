98f546b5038af51b6a5b1b9fc91f64a748310a57
Ruben <rubenyeghikyan@gmail.com>
2024-02-12 17:07:10 +0400

    Change time tracking based on API changes

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:9000/api/v1/";
+export const API_URL = "https://hrinspectorapi.com/api/v1/";
--- src/types/timetracking.ts
+++ src/types/timetracking.ts
@@ -1,6 +1,17 @@
+export interface IDailyWorkedHours {
+  startTime: number;
+  endTime: number;
+  id: string;
+}
+
+export type UserActivitiesDailyWorkedHours = Record<
+  number,
+  IDailyWorkedHours[]
+>;
+
 export interface ITimeTracking {
+  dailyWorkedHours: UserActivitiesDailyWorkedHours;
   totalWorkedHours: number;
-  dailyWorkedHours: Record<number, number>;
   salaryForHours?: number;
   salaryForMonth?: number;
 }
